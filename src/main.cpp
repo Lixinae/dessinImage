0b5ecb7695410616c7ff690004d25149f4658dd5
Vahen <naeos@laposte.net>
2018-02-17 16:41:31 +0100

    - Ajout du debut de l'implementation en SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,54 @@
 #include <sstream>
 #include <figure/image.hpp>
 #include <condition/condition.hpp>
-#include <SDL/SDL.h>
 
 using namespace std;
 using namespace figure;
 using namespace condition;
 
+#include <SDL.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 void mainFunction() {
-    if (SDL_Init(SDL_INIT_VIDEO) == -1) {
-        cerr << "Erreur d'initialisation de la SDL" << endl;
-        exit(EXIT_FAILURE);
-    }
-    SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_RESIZABLE | SDL_DOUBLEBUF);
+    //The window we'll be rendering to
+    SDL_Window *window = NULL;
 
-    SDL_Event event{};
+    //The surface contained by the window
+    SDL_Surface *screenSurface = NULL;
 
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == nullptr) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
 
-    auto continu = true;
-    while (continu) {
-        SDL_WaitEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-                continu = false;
-                break;
-            default:
-                break;
+            //Fill the surface white
+            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+
+            //Wait two seconds
+            SDL_Delay(2000);
         }
-        /*
- * TODO
- * Do stuff here
- */
     }
 
+    //Destroy window
+    SDL_DestroyWindow(window);
 
+    //Quit SDL subsystems
     SDL_Quit();
+
 }
 
 int WinMain() {
