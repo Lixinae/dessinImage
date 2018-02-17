70cba8f6852ab1cd5012831de0c5f6aa5091c5ba
cdluznie <cdluznie@etud.u-pem.fr>
2018-02-17 15:26:58 +0100

    cercle

--- include/cercle.hpp
+++ include/cercle.hpp
@@ -8,11 +8,14 @@ namespace figure {
 using namespace std;
 
 class Cercle : public Figure {
+	
 public:
+    
     static Cercle temoin;
 
-    explicit Cercle(const Point &centre = Point(0, 0), int rayon = 0)
-            : _centre(centre), _rayon(rayon) {}
+    explicit Cercle();
+    explicit Cercle(const Point &centre);
+    explicit Cercle(const Point &center, int rayon);
 
     std::shared_ptr<Figure> copy() const override;
 
@@ -27,8 +30,10 @@ public:
     void afficher(ostream &os = cout) const override;
 
 private:
+    
     Point _centre;
     int _rayon;
+
 };
 
 }
--- src/cercle.cpp
+++ src/cercle.cpp
@@ -1,37 +1,28 @@
-/**
- * auteurs          : Michel Landschoot
- * mail             : direction@landsnet.com
- * date de cr?ation : 2013-12-21
- * description      : impl?m?ntation d'une classe d?crivant un cercle dans une hi?rachie de figures
- */
-
 #include "cercle.hpp"
 
 namespace figure {
 
-/**
- * Le cercle t?moin est une variable de classe
- */
 Cercle Cercle::temoin = Cercle(Point(0, 0), 0);
 
+Cercle::Cercle() : Cercle(Point(0,0), 0) {
 
-/**
- * Fonction virtuelle de copie
- */
+}
+Cercle::Cercle(const Point &centre) : Cercle(centre, 0) {
+
+}
+
+Cercle::Cercle(const Point &centre, int rayon) : _centre(centre), _rayon(rayon) {
+
+}
+
 shared_ptr<Figure> Cercle::copy() const {
     return std::shared_ptr<Figure>(new Cercle(this->_centre, this->_rayon));
 }
 
-/**
- * D?placement-translation de valeur le point p
- */
 void Cercle::deplacer(const Point &p) {
     _centre += p;
 }
 
-/**
- * Le dessin se limite ? un affichage
- */
 void Cercle::dessiner(ostream &os) const {
     os << *this << endl;;
 }
