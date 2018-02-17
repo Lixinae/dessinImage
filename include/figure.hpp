62eaaed65d8de8961da68f50b87151766ec0c660
cdluznie <cdluznie@etud.u-pem.fr>
2018-02-17 15:13:14 +0100

    figure

--- include/figure.hpp
+++ include/figure.hpp
@@ -10,35 +10,66 @@ namespace figure {
 
 using namespace std;
 
-// classe abstraite de base
 class Figure {
 
 public:
-    /**
-     * Il est impossible de rendre les constructeurs de copie virtuels.
-     * Par suite, une fonction virtuelle de copy est d?finie qui permet
-     * la simulation de constructeurs virtuels.
-     */
-    virtual std::shared_ptr<Figure> copy() const = 0;
 
+   /**
+	* Realise une copie profonde de la figure
+	* \return une copie profonde de la figure
+	*/ 
+    virtual std::shared_ptr<Figure> copy() const = 0; // TODO figure *
+
+   /**
+    * Detruit la figure
+    */
     virtual ~Figure() {};
 
-    virtual void deplacer(const Point &trans) = 0;
+   /**
+    * Deplace la figure
+    * \param trans : translation
+    */
+    virtual void deplacer(const Point &trans) = 0; // TODO operation(matrice2d)
 
-    virtual void dessiner(ostream &os = cout) const = 0;
+   /**
+    * Dessine la figure
+    * \param os : flux
+    */
+    virtual void dessiner(ostream &os = cout) const = 0; //TODO rm os
 
+   /**
+    * Affiche la figure dans un flux
+    * \param os : le flux
+    * \return le flux
+    */
     virtual void afficher(ostream &os) const = 0;
+    
+   /**
+    * Calcule la surface de la figure
+    * \return la surface de la figure
+    */
+    virtual double surface() const = 0; //TODO rm -> perimetre aire
 
-    virtual double surface() const = 0;
-
+   /**
+    * Teste l'egalite de deux figures
+    * \param f : figure a tester
+    * \return true si les figures sont les memes, faux sinon
+    */
     bool operator==(const Figure &f) const {
-        return (this == &f);
+        return (this == &f); //TODO egalite memoire ?
     }
 
+   /**
+    * Ecrit une figure dans un flux
+    * \param os : le flux
+    * \param figure : la figure a ecrire
+    * \return le flux
+    */
     friend ostream &operator<<(ostream &os, const Figure &figure) {
         figure.afficher(os);
         return os;
     }
+
 };
 
 }
