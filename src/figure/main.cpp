3c7b4f6008d9fa2313b18e2e9817296cdbe51499
cdluznie <cdluznie@etud.u-pem.fr>
2018-03-18 18:47:01 +0100

    creation figure test

--- src/figure/main.cpp
+++ src/figure/main.cpp
@@ -1,7 +1,7 @@
 #include <sstream>
-#include <figure/cercle.hpp>///////
-#include <figure/triangle.hpp>////////////
-#include <figure/rectangle.hpp>////////////
+#include <figure/cercle.hpp>///////rm
+#include <figure/triangle.hpp>////////////rm
+#include <figure/rectangle.hpp>////////////rm
 #include <figure/filtrage.hpp>
 #include <dessin/SDLdessin.hpp>
 #include <figure/serialisationJSON.hpp>
@@ -17,54 +17,46 @@ namespace figure {
 
 	void main() {
 
-//		auto x = Figure::creerFigures(5);
-//		std::cerr << x.size() << std::endl;
+		const Serialisation & serialisation = SerialisationJSON();
 
-		SDLdessin dessin(800, 600);
+		SDLdessin dessin(800, 800);
 		dessin.initialise();
-
-        Ligne l(Point(10, 10), Point(500, 200));
-        Cercle c(Point(50, 50), 20);
-        Triangle t(Point(10, 10), Point(200, 10), Point(50, 300));
-        Rectangle r(Point(100, 100), Point(500, 200));
-
+	
+		/*
+        Ligne l(Point(420, 520), Point(730, 620));
+        Ligne l2(Point(200, 500), Point(340, 640));
+        Ligne l3(Point(340, 500), Point(200, 640));
+        Cercle c(Point(140, 180), 80);
+        Cercle c2(Point(580, 300), 140);
+        Cercle c3(Point(580, 300), 80);
+        Cercle c4(Point(580, 110), 40);
+        Cercle c5(Point(480, 135), 40);
+        Cercle c6(Point(408, 210), 40);
+        Triangle t(Point(50, 290), Point(250, 290), Point(100, 580));
+        Rectangle r(Point(400, 500), Point(750, 640));
         Image image;
         image.ajouter(c);
         image.ajouter(t);
-
-        image.dessiner(dessin);
-
+        image.ajouter(l2);
+        image.ajouter(l3);
         Image image2;
         image2.ajouter(r);
         image2.ajouter(l);
-
+        Image image3;
+        image2.ajouter(c2);
+        image2.ajouter(c3);
+        image2.ajouter(c4);
+        image2.ajouter(c5);
+        image2.ajouter(c6);
         image.ajouter(image2);
-//
-//		c.dessiner(dessin);
-//        t.dessiner(dessin);
-//		dessin.attendTouche();
-//
-//		Matrice2D transformations = Matrice2D::translation(10, 220) * Matrice2D::rotation(-0.8);
-//		t.tranformation(transformations);
-//
-//		dessin.nettoie();
-//		c.dessiner(dessin);
-//		t.dessiner(dessin);
-//		r.dessiner(dessin);
-//		dessin.attendTouche();
+        image.ajouter(image3);
+        serialisation.sauvegarde("./data/demo.json", image);
+        */
 
+        shared_ptr<Figure> figure = serialisation.charge("data/demo.json");
+        figure->dessiner(dessin);
         dessin.attendTouche();
-        dessin.nettoie();
-        SerialisationJSON serialisationJSON;
-        serialisationJSON.sauvegarde("./data/demo.json", image);
-
-        Figure *fig = serialisationJSON.charge("./data/demo.json");
-        fig->dessiner(dessin);
-        dessin.attendTouche();
-
-        Filtrage filtrage;
-        EstPetite estPetite(10);
-//        cout << filtrage.compterSi(image,estPetite) << endl;
+        
     }
 
 }
