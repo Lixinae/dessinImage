4eeab46eb2b2274fc8038deb8e7a757ecbffe501
cdluznie <cdluznie@etud.u-pem.fr>
2018-03-18 19:05:56 +0100

    mise a jour du fichier de figure

--- src/figure/main.cpp
+++ src/figure/main.cpp
@@ -34,24 +34,24 @@ namespace figure {
         Cercle c6(Point(408, 210), 40);
         Triangle t(Point(50, 290), Point(250, 290), Point(100, 580));
         Rectangle r(Point(400, 500), Point(750, 640));
-        Image image;
+        Image image(Point(400,400));
         image.ajouter(c);
         image.ajouter(t);
         image.ajouter(l2);
         image.ajouter(l3);
-        Image image2;
+        Image image2(Point(575,570));
         image2.ajouter(r);
         image2.ajouter(l);
-        Image image3;
-        image2.ajouter(c2);
-        image2.ajouter(c3);
-        image2.ajouter(c4);
-        image2.ajouter(c5);
-        image2.ajouter(c6);
+        Image image3(Point(580,300));
+        image3.ajouter(c2);
+        image3.ajouter(c3);
+        image3.ajouter(c4);
+        image3.ajouter(c5);
+        image3.ajouter(c6);
         image.ajouter(image2);
         image.ajouter(image3);
         serialisation.sauvegarde("./data/demo.json", image);
-        */
+		*/
 
         shared_ptr<Figure> figure = serialisation.charge("data/demo.json");
         figure->dessiner(dessin);
